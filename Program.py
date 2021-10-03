74d10f022e5c97cebefe07763693cf6d0b76fc8f
Gavriel Jaffe <gavrielmn@gmail.com>
2021-10-03 20:02:48 +0300

    adding Factorial numbers as well to the program

--- Program.py
+++ Program.py
@@ -3,5 +3,15 @@ def main ():
     for i in range(101) : 
         sum+=i
         print ("all of number's : ",sum)
+        if((i==5)or(i==6)or(i==7)or(i==8)):
+            x =azert(i)
+            print("the azert of the number is :  ",x,i)
+
+def azert(number):
+    prodect=1
+    for j in  range (number):
+        prodect =prodect * (j+1)
+    return prodect
+
 if __name__=="__main__":     
     main()
