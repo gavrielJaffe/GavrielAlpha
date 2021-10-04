b100e065591ec7b3be703d284e30f05cedb25397
gavrielJaffe <55511597+gavrielJaffe@users.noreply.github.com>
2021-10-04 15:06:09 +0300

    Update Program.py

--- Program.py
+++ Program.py
@@ -5,15 +5,8 @@ def notural(x):
     else:
         return True
 
-def azert(number):
-    prodect=1
-    for j in  range (number):
-        prodect =prodect * (j+1)
-    return prodect
-
 def main (): 
     sum=1
-    
     for i in range(101) : 
         sum+=i   
 print ("all of number's : ",sum)
@@ -22,15 +15,6 @@ print("for the number 6:" ,notural(6))
 print("for the number 7"  ,notural(7))
 print("for the number 14" ,notural(14))
 print("for the number 152",notural(152))
-print("for the number 60693",notural(60693))     
-for i in range(101): 
-    sum+=i
-    print ("all of number's : ",sum)
-        
-    if((i==5)or(i==6)or(i==7)or(i==8)):
-        x =azert(i)
-        print("the azert of the number is :  ",x,i)
-
-
+print("for the number 60693",notural(60693))             
 if __name__=="__main__":     
     main()
