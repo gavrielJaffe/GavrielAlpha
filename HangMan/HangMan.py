abccd83c47b03dd1f4a094ff5bbae9017477dc30
GavrielJaffe <gavrielmn@gmail.com>
2021-10-05 18:59:08 +0300

    adding most of HangMan

--- HangMan/HangMan.py
+++ HangMan/HangMan.py
@@ -1,24 +1,32 @@
 import random
+from typing import Counter
 
-def fun():{
-    
-}
-def main():    
-    GuessWords = ["alpha", "is", "great","team"]
-    print ("words",GuessWords)
+def main():
+    counter=8    
+    GuessWords = ("alpha", "is", "great","team")
     rnd=random.randrange(0, 4)
     GameWord=GuessWords[rnd]
-    size=len(GameWord)
-    underLine = "_" * size
-    for i in range(8):
-        print("your information on the words currntly is  : ",underLine)
-        #ניתן להפו לפונקיצה אחת בודקת ומחזירה לחסוך בזמן ולא  בתור פור?נקודה למחשבה -יעילות
-        print("entear a lette between a-z: ")
-        letter=input()
-        while(letter.isalpha()):        
-            print("can't be anything else than english abc letters")
-            letter=input()
-        print("Great it was between the abc letters ")
-        
-if(__name__=="__main__"):
+    list_guess=["_"] * len(GameWord)
+    #as long as the player have life or did not win the game.
+    while(( (counter!=0) or (GameWord=="".join(list_guess)))):
+        print("your information on the words currntly is: ",list_guess)
+        letter=input("entear one of letter between a-z:").lower()
+        #  while input invalid,asking for a good input
+        while not (letter.isalpha() and len(letter)==1):     
+            letter=input("can't be anything else than single english abc letters try again: ").lower()
+        #need to put player guss in place ,
+        if(letter in GameWord):
+                for i in range (len(GameWord)):
+                #compare this letter to every guessed by user 
+                    if(GameWord[i]==letter):
+                       list_guess[i]=letter
+                print("The current situation :",list_guess)
+        else:
+            counter=counter-1
+            print("Sorry you are incorrect")
+    if(GuessWords=="".join(list_guess)):
+        print("good,you got avery thing right")
+
+
+if (__name__=="__main__"):  
     main()
