b86e0a47a622bc2b611a977343865da6cff4a0ea
GavrielJaffe <gavrielmn@gmail.com>
2021-10-06 14:33:07 +0300

    adding the fires part of game

--- SuperMarket/SuperMarket.py
+++ SuperMarket/SuperMarket.py
@@ -1,17 +1,32 @@
+
 def main():
 
-    thisdict = {
-    "brand": "Ford",
-    "model": "Mustang",
-    "year": 1964,
-    "year": 2020 }
-                       
-    print(thisdict)
-
-
-
-
-
+   number=input("To add a product click 1,for Deleting a product click 2, to cart click 3 ")
+   while((number!="3")):
+        print("type is : ",type(number))
+        #Add a product   
+        if(number=="1"):
+            print("adding")
+            break
+        #delete a product     
+        elif(number=="2"):
+            print("deleting")
+            break
+        #end of shopping     
+        elif(number=="3"):
+            break
+        else:
+            number=input("could be numbers only 1,2,3 , To add a product click 1,for Deleting a product click 2, to cart click 3:  ")
 
 if __name__=='__main__':
     main()
+
+#   thisdict = {
+#     "brand": "Ford",
+#     "model": "Mustang",
+#     "year": 1964,
+#     "year": 2020 }
+                       
+#     print(thisdict)
+
+
