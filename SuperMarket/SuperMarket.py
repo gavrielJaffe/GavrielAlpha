0baefa29113c2508ac249c047d65f1813210f7f3
GavrielJaffe <gavrielmn@gmail.com>
2021-10-07 11:32:20 +0300

    add new functino

--- SuperMarket/SuperMarket.py
+++ SuperMarket/SuperMarket.py
@@ -1,30 +1,34 @@
- import re
+def add_prodact(all_products,new_product):
+    all_products[len(all_products)+1]=new_product
+#dic is pass by refrence, no need for return 
 
+
+
+    
 def main():
+   PriceInCart = 0
    number=input("To add a product click 1,for Deleting a product click 2, to cart click 3 ")
+   all_products={}
    while((number!="3")):
-        print("type is : ",type(number))
         #Add a product   
         if(number=="1"):
-            #get name of Product 
-            product=input("enter name of product: ")
-            #get price in 'int' only 
+            #get name of Product,price and quantity in 'int' only    
+            product_name=input("enter name of product: ")
             price=input("enter price of product: ") 
             while not(price.isdigit()):
                 price=input("enter price of product: ") 
-            #get unit number 'int' only    
-            unit=input("enter unit number to purchase: ")
-            while not( unit.isdigit()):
-                unit=input("enter unit number to purchase: ")
-            
+            quantity=input("enter quantity number to purchase:")
+            while not( quantity.isdigit()):
+                quantity=input("enter quantity number to purchase: ")
 
-            
-    #dic={"penter roductName":"price","unit" } 
-
-
-            print("adding")
+            if (product_name in all_products):
+                print("you have this product alredy  25\n")
+                
+            new_product={"name" :product_name, "price_prodct":price,"unit_number":quantity}            
+            add_prodact(all_products,new_product)
+            if (product_name in new_product):
+                print("you have this product alredy 30\n")
             break
-        #delete a product     
         elif(number=="2"):
             print("deleting")
             break
@@ -37,12 +41,5 @@ def main():
 if __name__=='__main__':
     main()
 
-#   thisdict = {
-#     "brand": "Ford",
-#     "model": "Mustang",
-#     "year": 1964,
-#     "year": 2020 }
-                       
-#     print(thisdict)
 
 
