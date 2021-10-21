e1956c1cbaa7b1b52fd1d255b84ddfe5e0f2db44
GavrielJaffe <gavrielmn@gmail.com>
2021-10-21 12:20:56 +0300

    creat basic stuff

--- turns_game/Hero.py
+++ turns_game/Hero.py
@@ -1,11 +1,23 @@
-from _typeshed import Self
 
-
-class Hero(Self):
-    self.hp=hp
-    self.damage=damage
-    self.level=level
-    self.conis=conis
+class Hero:
+    def __init__(self):
+        self.hp=hp
+        self.damage=damage
+        self.level=level
+        self.coins=coins
 
     def Constructor():
-        
+        print("")
+    def Heal():
+        print("")
+    def level_up():
+        print("")
+    def attack_hero():
+        print("")
+    def defend():
+        print("")
+    def reduce_health():
+        print("")
+    def choose_action():
+        print("")
+    
---
+++ turns_game/monster.py
@@ -0,0 +1,15 @@
+
+class Monster:
+    def __init__(self):
+        self.monster_name=monster_name
+        self.monster_hp=monster_hp
+        self.monster_damage=monster_damage
+        self.monster_level=monster_level
+
+    def constructor_monster():
+        print("")
+    def attack_monster():
+        print("")
+    def reduce_health_monster():
+        print("")
+    
