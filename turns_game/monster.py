73e90b8e70d7d800e0717012119b1ce2d8545da2
GavrielJaffe <gavrielmn@gmail.com>
2021-11-08 22:45:06 +0200

    adding to reduce_health_monster function -monster

--- turns_game/monster.py
+++ turns_game/monster.py
@@ -1,11 +1,13 @@
 
+from _typeshed import Self
 import random
+from Hero import Hero 
 class Monster:
     def __init__(self):
-        self.monster_hp= self.monster_hp
+        self.monster_name= self.monster_name
+        self.monster_hp=self.monster_hp
         self.monster_damage= self.monster_damage
         self.monster_level=self.monster_level
-        self.monster_name= self.monster_name
 
     def constructor_monster(monster_name,hero,monster):
         # get level of monster.
@@ -19,7 +21,11 @@ class Monster:
         #need to reduce helth monster to the hero.or monster to hero.
         monster.reduce_health_monster()
         print("")
-    def reduce_health_monster():
-        print("")
-        
+    def reduce_health_monster(hero,monster):
+        monster.monster_hp = hero.damage - monster.hp
+        if (monster.monster_hp<=0):
+            monster.monster_hp=0
+            #do we need to create a new monster hear ? 
+        return monster.monster_hp
+
     
