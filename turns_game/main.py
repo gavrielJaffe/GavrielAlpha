aff3b242d06375eac271d968bd10c9b20cdd91e8
GavrielJaffe <gavrielmn@gmail.com>
2021-11-11 08:13:26 +0200

    adding main

--- turns_game/main.py
+++ turns_game/main.py
@@ -1,29 +1,23 @@
-
-
-from typing import Counter
-from Hero import Hero
-
 from monster import Monster
 from Hero import Hero
 
 def main():
-    obj_hero=Hero()
     cunt_dead=0
-    monster_resat=5
+    monster_reset=5
 
-    #need to inized the moster
-    obj_monster=Monster(x,y,z,"bogis")
-    obj_hero.choose_action(obj_hero,obj_monster)
-    if (obj_monster.monster_hp<0):
-        cunt_dead=cunt_dead+1
-        #creating a new moster stronger.
-        obj_monster=Monster(monster_resat+cunt_dead,monster_resat+cunt_dead,monster_resat+cunt_dead)
+    obj_hero=Hero()
+    obj_monster=Monster(monster_reset,monster_reset,monster_reset,"bogis")
+    print(obj_hero,"obj hero")
+    while( obj_hero.hp!= 0 ):
+        obj_hero.choose_action(obj_hero,obj_monster)
+        if (obj_monster.monster_hp<0):
+            cunt_dead=cunt_dead+1
+            #creating a new moster, stronger.
+            obj_monster=Monster(monster_reset+cunt_dead,monster_reset+cunt_dead,monster_reset+cunt_dead)
         #attack the hero.
         obj_monster.attack(obj_hero,obj_monster)
-        
-
-
-        print ("hi from 13")
-    print(obj_hero,"obj hero")
+        if(obj_hero.hp<=0):
+            print("you lost in the game")
+            break
     if(__name__=="__main__"):
         main()
